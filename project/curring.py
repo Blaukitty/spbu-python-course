fcd97a6ca3297fa9bdd1ca6dfbd9d21aa59eaa42
Rodionova Ksenia <st132496@student.spbu.ru>
2025-10-14 18:44:02 +0300

    Create curring.py

---
+++ project/curring.py
@@ -0,0 +1,47 @@
+from typing import Any, Callable
+
+
+def curry_explicit(function: Callable[..., Any], arity: int) -> Callable[..., Any]:
+    """
+    Transforms a function into a curried version.
+    
+    Args:
+        function: The function to be curried
+        arity: The number of arguments the function expects
+        
+    Returns:
+        A curried version of the input function
+    """
+    def curry(*args: Any) -> Any:
+        # If enough arguments have been accumulated, call the original function
+        if arity <= len(args):
+            return function(*args)
+        else:
+            # Otherwise return a new function that remembers current arguments
+            def new(*new_args: Any) -> Any:
+                all_ar = args + new_args
+                return curry(*all_ar)
+            return new
+
+    return curry
+
+
+def uncurry_explicit(function: Callable[..., Any], arity: int) -> Callable[..., Any]:
+    """
+    Transforms a curried function back into normal form.
+    
+    Args:
+        function: The curried function to uncurry
+        arity: The number of arguments the original function expected
+        
+    Returns:
+        An uncurried version of the input function
+    """
+    def uncurry(*args: Any) -> Any:
+        # Sequentially apply all arguments to the curried function
+        res = function
+        for ar in args:
+            res = res(ar)
+        return res
+
+    return uncurry
