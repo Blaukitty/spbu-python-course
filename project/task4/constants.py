f0bce2c4a2cb393d8ae0e2d1f62eb28e2d925321
Rodionova Ksenia <st132496@student.spbu.ru>
2025-11-25 13:02:06 +0300

    Create constants.py

---
+++ project/task4/constants.py
@@ -0,0 +1,31 @@
+from enum import Enum
+
+class StrategyType(Enum):
+    DALAMBER = 1
+    MARTINGEIL = 2
+    ALL_CAPITAL = 3
+
+class Color(Enum):
+    RED = 'red'
+    BLACK = 'black'
+    GREEN = 'green'
+
+class BetType(Enum):
+    SINGLE = "single"
+    COLOR = "color"
+    DOZEN = "dozen"
+    EVEN_ODD = "even_odd"
+    RANGE = "range"
+
+# Коэффициенты выплат
+PAYOUTS = {
+    BetType.SINGLE: 35,
+    BetType.COLOR: 2,
+    BetType.DOZEN: 3,
+    BetType.EVEN_ODD: 2,
+    BetType.RANGE: 2
+}
+
+# Базовые настройки
+BASE_BET = 100
+INITIAL_MONEY = 1000
